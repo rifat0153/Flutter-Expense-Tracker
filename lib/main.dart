c5619c6848602a2dd38f93118561864c38167237
Mahbubur Rahman <rifat0153@gmail.com>
2021-03-22 03:00:03 +0600

    transaction class created

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import './transaction.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -20,6 +21,13 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+        id: 't1', title: 'New Shoes', amount: 67.33, date: DateTime.now()),
+    Transaction(
+        id: 't2', title: 'New Bike', amount: 3000, date: DateTime.now()),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,15 +36,18 @@ class MyHomePage extends StatelessWidget {
       ),
       body: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             Container(
               width: double.infinity,
               child: Card(
+                color: Colors.pink,
                 child: Text('Card'),
                 elevation: 5,
               ),
             ),
             Card(
+              color: Colors.red,
               child: Container(width: 100, child: Text('List of data')),
               elevation: 5,
             )
